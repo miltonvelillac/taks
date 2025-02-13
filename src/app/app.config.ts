6b8036cff4ca5e8f14b59fec7b5e5cd91097e51b
Milton Velilla <miltonvelilla@miltonvelillal.local>
2025-02-13 13:18:13 -0500

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,32 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "tasks-58eab",
+      appId: "1:1056153975927:web:7ec111a7aed5546146da72",
+      storageBucket: "tasks-58eab.firebasestorage.app",
+      apiKey: "AIzaSyCTTUkjrUskRc30flerYNvdUue-91bmhtk",
+      authDomain: "tasks-58eab.firebaseapp.com",
+      messagingSenderId: "1056153975927",
+      measurementId: "G-V5PFXKB53T" 
+    })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ]
 };
